a561c63e5fd276a7772c3cc6c89fbb656a9e0b4a
aries5533 <olumokomoses94@gmail.com>
2024-01-21 16:28:31 +0100

    updated variable

--- variables.tf
+++ variables.tf
@@ -5,21 +5,10 @@ variable "clients" {
   }))
   default = [
     
-    { client_name = "Client116", subdomain = "client6" },
-    { client_name = "Client117", subdomain = "client7" },
-    { client_name = "Client118", subdomain = "client8" },
-    { client_name = "Client119", subdomain = "client9" },
-    { client_name = "Client1110", subdomain = "client10" },
-    { client_name = "Client1111", subdomain = "client11" },
-    { client_name = "Client1112", subdomain = "client12" },
-    { client_name = "Client1113", subdomain = "client13" },
-    { client_name = "Client1114", subdomain = "client14" },
-    { client_name = "Client1115", subdomain = "client15" },
-    { client_name = "Client1116", subdomain = "client16" },
-    { client_name = "Client1117", subdomain = "client17" },
-    { client_name = "Client1118", subdomain = "client18" },
-    { client_name = "Client1119", subdomain = "client19" },
-    { client_name = "Client2110", subdomain = "client20" },
+    { client_name = "Client1", subdomain = "client1" },
+    { client_name = "Client2", subdomain = "client2" },
+    { client_name = "Client3", subdomain = "client3" },
+    # Add more clients as needed
 
   ]
 }
